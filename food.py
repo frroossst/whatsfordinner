5f4bce95bfefc98ac6b67960332f7d9979da2474
frroossst <adhyanpatel@gmail.com>
2020-10-16 17:01:18 +0530

    cleaned up

--- food.py
+++ food.py
@@ -1,5 +1,7 @@
+#importing library
 import random
 import getpass
+#login function
 def passw():
     r=getpass.getpass("password : ")
     rp='4883'
@@ -7,18 +9,29 @@ def passw():
         pass
     else:
         quit()
+#variables
 a=0
-li=['mag','tuver','masoor','thebra','thepla/masala bhakri','paratha','vinu bhai khichdi','tuver ma dhokdi','chora','pav bhaji','chole','rajma','vaal','turiya patra','bhagat muthiya','vaal ni daard','daad bhaat','masala khichdi','khada masala khichdi','frankie','burito','chatpata aloo naan','ragda petis','idli sambhar','dosa','ringad papdi','dana ringad','dhokla','daad ma dhokdi']
+cont='y'
+#dataset
+li=['mag','tuver','masoor','thebra','thepla/masala bhakri','paratha','vinu bhai khichdi','tuver ma dhokdi','chora','pav bhaji','chole','rajma','vaal','turiya patra','bhagat muthiya','vaal ni daard','daad bhaat','masala khichdi','khada masala khichdi','frankie','burito','chatpata aloo naan','ragda petis','idli sambhar','dosa','ringad papdi','dana ringad','dhokla','daad ma dhokdi','dudhi chana','paneer angara']
+#login
 passw()
-print("1. one food item")
-print("2. multiple food items")
-print()
-ch=int(input("enter choice : "))
-if ch==1:
-    print(random.choice(li))
-elif ch==2:
-    a=int(input("how many food choices? "))
-    x=random.sample(li,a)
+#UI
+while cont=='y' or cont=='Y':
+    print("1. one food item")
+    print("2. multiple food items")
     print()
-    for i in x:
-        print(i)
+    ch=int(input("enter choice : "))
+    if ch==1:
+        print(random.choice(li))
+        cont=input("do you want to continue? y/n ")
+    elif ch==2:
+        a=int(input("how many food choices? "))
+        x=random.sample(li,a)
+        print()
+        for i in x:
+            print(i)
+        cont=input("do you want to continue? y/n ")
+
+
+
