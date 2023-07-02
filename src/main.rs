e171019fbc9733d3975a6228da1d4815c3cca299
Adhyan H <adhyanpatel@gmail.com>
2023-07-02 16:42:43 -0600

    generate a HashSet of ingredients to buy

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use whatsfordinner::grocery;
+use whatsfordinner::{grocery, measurements::Measurements};
+use std::collections::HashSet;
 
 fn main() {
     // load all recipes into memory
@@ -7,10 +8,17 @@ fn main() {
     let mut picked = grocery::pick_recipes(&mut db, 10);
     picked.sort();
 
-    println!("{:#?}", picked);
     println!("picked {}/{} recipes", picked.len(), db.len());
 
-    for i in picked.iter().map(|x| x.ingredients.iter()).flatten() {
-        println!("{:?}", i);
+    let mut ingredients_to_buy: HashSet<String, _> = HashSet::new();
+
+    // make a list of needed ingredients
+    for i in picked {
+        for j in i.ingredients.keys() {
+            ingredients_to_buy.insert(j.to_string());
+        }
     }
+
+    println!("need to buy {} ingredients", ingredients_to_buy.len());
+    println!("{:?}", ingredients_to_buy);
 }
